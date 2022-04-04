5afd63f8545b20cc787b3a3ec11b5c8a251bad2e
nasremp <63264076+nasremp@users.noreply.github.com>
2022-04-04 20:56:13 +0200

    Update student.py

--- student.py
+++ student.py
@@ -1,54 +1,72 @@
 import numpy as np
 
 
+from skimage import filters, feature, img_as_int
+
 def get_interest_points(image, feature_width):
     """
     Returns interest points for the input image
-
     (Please note that we recommend implementing this function last and using cheat_interest_points()
     to test your implementation of get_features() and match_features())
-
     Implement the Harris corner detector (See Szeliski 4.1.1) to start with.
     You do not need to worry about scale invariance or keypoint orientation estimation
     for your Harris corner detector.
     You can create additional interest point detector functions (e.g. MSER)
     for extra credit.
-
     If you're finding spurious (false/fake) interest point detections near the boundaries,
     it is safe to simply suppress the gradients / corners near the edges of
     the image.
-
     Useful functions: A working solution does not require the use of all of these
     functions, but depending on your implementation, you may find some useful. Please
     reference the documentation for each function/library and feel free to come to hours
     or post on Piazza with any questions
-
         - skimage.feature.peak_local_max (experiment with different min_distance values to get good results)
         - skimage.measure.regionprops
-
-
     :params:
     :image: a grayscale or color image (your choice depending on your implementation)
     :feature_width:
-
     :returns:
     :xs: an np array of the x coordinates of the interest points in the image
     :ys: an np array of the y coordinates of the interest points in the image
-
     :optional returns (may be useful for extra credit portions):
     :confidences: an np array indicating the confidence (strength) of each interest point
     :scale: an np array indicating the scale of each interest point
     :orientation: an np array indicating the orientation of each interest point
-
     """
 
-    # TODO: Your implementation here! See block comments and the project webpage for instructions
+    alpha = 0.06
+    threshold = 0.01
+    min_distance = 3
 
-    # These are placeholders - replace with the coordinates of your interest points!
-    xs = np.zeros(1)
-    ys = np.zeros(1)
+    #Sobel
+    I_x = filters.sobel_v(image)
+    I_y = filters.sobel_h(image)
 
-    return xs, ys
+    #calculate Gxx, Gxy, Gyy
+    I_xx = np.square(I_x)
+    I_xy = np.multiply(I_x, I_y)
+    I_yy = np.square(I_y)
+
+
+    listC = np.zeros_like(image)
+
+    xs=[]
+    ys=[]
+    # caculate C matrix
+    for y in range(0,rows-feature_width,2):
+        for x in range(0,cols-feature_width,2):
+            Sxx = np.sum(Ixx[y:y+feature_width+1, x:x+feature_width+1])
+            Syy = np.sum(Iyy[y:y+feature_width+1, x:x+feature_width+1])
+            Sxy = np.sum(Ixy[y:y+feature_width+1, x:x+feature_width+1])
+            #Find determinant and trace, use to get corner response
+            detH = (Sxx * Syy) - (Sxy**2)
+            traceH = Sxx + Syy
+            R = detH - a*(traceH**2)
+            #If corner response is over threshold, it is a corner
+            if R > threshold:
+                xs.append(x + int(feature_width/2 -1))
+                ys.append(y + int(feature_width/2 -1))
+    return np.asarray(xs), np.asarray(ys)
 
 
 def get_features(image, x, y, feature_width):
