84f1ff9f1b89cdc5085e2dbffbd011219da23a03
nasremp <63264076+nasremp@users.noreply.github.com>
2022-04-17 22:33:46 +0200

    Update student.py

--- student.py
+++ student.py
@@ -5,7 +5,6 @@ import cv2
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use("TkAgg")
-
 from scipy import ndimage
 from skimage import filters, feature, img_as_int
 from math import floor
@@ -42,8 +41,8 @@ def get_interest_points(image, feature_width):
     """
 
     
-    alpha = 0.05
-    threshold = 0.004
+    alpha = 0.06
+    threshold = 0.005
     stride = 2
     sigma = 0.2
     #coordinates of the interest points
@@ -86,13 +85,11 @@ def get_interest_points(image, feature_width):
 def get_features(image, x, y, feature_width):
     """
     Returns feature descriptors for a given set of interest points.
-
     To start with, you might want to simply use normalized patches as your
     local feature. This is very simple to code and works OK. However, to get
     full credit you will need to implement the more effective SIFT-like descriptor
     (See Szeliski 4.1.2 or the original publications at
     http://www.cs.ubc.ca/~lowe/keypoints/)
-
     Your implementation does not need to exactly match the SIFT reference.
     Here are the key properties your (baseline) descriptor should have:
     (1) a 4x4 grid of cells, each feature_width / 4 pixels square.
@@ -100,34 +97,26 @@ def get_features(image, x, y, feature_width):
         gradients in 8 orientations. Appending these histograms together will
         give you 4x4 x 8 = 128 dimensions.
     (3) Each feature should be normalized to unit length
-
     You do not need to perform the interpolation in which each gradient
     measurement contributes to multiple orientation bins in multiple cells
     As described in Szeliski, a single gradient measurement creates a
     weighted contribution to the 4 nearest cells and the 2 nearest
     orientation bins within each cell, for 8 total contributions. This type
     of interpolation probably will help, though.
-
     You do not have to explicitly compute the gradient orientation at each
     pixel (although you are free to do so). You can instead filter with
     oriented filters (e.g. a filter that responds to edges with a specific
     orientation). All of your SIFT-like feature can be constructed entirely
     from filtering fairly quickly in this way.
-
     You do not need to do the normalize -> threshold -> normalize again
     operation as detailed in Szeliski and the SIFT paper. It can help, though.
-
     Another simple trick which can help is to raise each element of the final
     feature vector to some power that is less than one.
-
     Useful functions: A working solution does not require the use of all of these
     functions, but depending on your implementation, you may find some useful. Please
     reference the documentation for each function/library and feel free to come to hours
     or post on Piazza with any questions
-
         - skimage.filters (library)
-
-
     :params:
     :image: a grayscale or color image (your choice depending on your implementation)
     :x: np array of x coordinates of interest points
@@ -137,12 +126,10 @@ def get_features(image, x, y, feature_width):
                     local SIFT-like feature will have an integer width and height).
     If you want to detect and describe features at multiple scales or
     particular orientations you can add input arguments.
-
     :returns:
     :features: np array of computed features. It should be of size
             [len(x) * feature dimensionality] (for standard SIFT feature
             dimensionality is 128)
-
     """
 
     # TODO: Your implementation here! See block comments and the project webpage for instructions
@@ -192,7 +179,7 @@ def get_features(image, x, y, feature_width):
             for j in range(feature_width // 4):
                 curr_mage = magnitude_window[i*feature_width//4: (i+1)*feature_width//4, j*feature_width//4:(j+1)*feature_width//4]
                 curr_dir = direction_window[i*feature_width//4: (i+1)*feature_width//4, j*feature_width//4:(j+1)*feature_width//4]
-                features[n, i, j] = np.histogram(curr_dir.reshape(-1), bins=8, range=(0, 2*np.pi), weights=curr_mag.reshape(-1))[0]
+                features[n, i, j] = np.histogram(curr_dir.reshape(-1), bins=8, range=(0, 2*np.pi), weights=curr_mage.reshape(-1))[0]
                     
 
     # reshape into 128 dim vector
@@ -220,52 +207,37 @@ def match_features(im1_features, im2_features):
     """
     Implements the Nearest Neighbor Distance Ratio Test to assign matches between interest points
     in two images.
-
     Please implement the "Nearest Neighbor Distance Ratio (NNDR) Test" ,
     Equation 4.18 in Section 4.1.3 of Szeliski.
-
     For extra credit you can implement spatial verification of matches.
-
     Please assign a confidence, else the evaluation function will not work. Remember that
     the NNDR test will return a number close to 1 for feature points with similar distances.
     Think about how confidence relates to NNDR.
-
     This function does not need to be symmetric (e.g., it can produce
     different numbers of matches depending on the order of the arguments).
-
     A match is between a feature in im1_features and a feature in im2_features. We can
     represent this match as a the index of the feature in im1_features and the index
     of the feature in im2_features
-
     :params:
     :im1_features: an np array of features returned from get_features() for interest points in image1
     :im2_features: an np array of features returned from get_features() for interest points in image2
-
     :returns:
     :matches: an np array of dimension k x 2 where k is the number of matches. The first
             column is an index into im1_features and the second column is an index into im2_features
     :confidences: an np array with a real valued confidence for each match
     """
 
-    # TODO: Your implementation here! See block comments and the project webpage for instructions
+    matches = []
+    confidences = []
+    threshold = 0.9
 
-    #matches = []
-    #confidences = []
-    #threshold = 0.7
-
-    #for r in range(im1_features.shape[0]):
-        #distance = np.sqrt(np.square(np.subtract(im1_features[r, :], im2_features)).sum(axis=1)) # get the distances between the fearures
-        #sort_index = np.argsort(distance)    #sort the distances from lower to higher
-        #if distance[sort_index[0]] / distance[sort_index[1]] < threshold:   # check for NNDR, if less than the threshold add it 
-            #matches.append([r, sort_index[0]])
-            #confidences.append(1.0 - distance[sort_index[0]]/distance[sort_index[1]])
-    #matches = np.asarray(matches)     
-    #confidences = np.asarray(confidences)
-
-    # These are placeholders - replace with your matches and confidences!
-
-    matches = np.zeros((1, 2))
-    confidences = np.zeros(1)
-
-
+    for r in range(im1_features.shape[0]):
+        distance = np.sqrt(np.square(np.subtract(im1_features[r, :], im2_features)).sum(axis=1)) # get the distances between the fearures
+        sort_index = np.argsort(distance)    #sort the distances from lower to higher
+        if distance[sort_index[0]] / distance[sort_index[1]] < threshold:   # check for NNDR, if less than the threshold add it 
+            matches.append([r, sort_index[0]])
+            confidences.append(1.0 - distance[sort_index[0]]/distance[sort_index[1]])
+    matches = np.asarray(matches)     
+    confidences = np.asarray(confidences)    
+    
     return matches, confidences
